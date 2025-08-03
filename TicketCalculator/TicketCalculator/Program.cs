9363ce404b47ccdba210b743bed86451da352d21
Emmanuel-Duodu <osae20002@gmail.com>
2025-08-03 16:16:44 +0000

    First commit for Ticket Calculator

---
+++ TicketCalculator/TicketCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        double ticketPrice;
+
+        if (age >= 65 || age <= 12)
+        {
+            ticketPrice = 7.00;
+        }
+        else
+        {
+            ticketPrice = 10.00;
+        }
+
+        Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+    }
+}
