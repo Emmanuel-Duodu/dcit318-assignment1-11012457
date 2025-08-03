049b1756204ae28758a25d4efcab8d399d63c719
Emmanuel-Duodu <osae20002@gmail.com>
2025-08-03 23:39:01 +0000

    First commit for triangle Identifier

---
+++ TriangleIdentifier/TriangleIdentifier/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the length of side 1:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the length of side 2:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the length of side 3:");
+        double side3 = Convert.ToDouble(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+        {
+            Console.WriteLine("The triangle is Equilateral.");
+        }
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
+        {
+            Console.WriteLine("The triangle is Isosceles.");
+        }
+        else
+        {
+            Console.WriteLine("The triangle is Scalene.");
+        }
+    }
+}
